186517a55a4150e290e1a7476c5b963d3ac1e8fa
jgabriel1 <josegabriel@det.ufc.br>
2020-08-30 20:27:18 -0300

    Added default value for postgres url.

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const POSTGRES_URL = process.env.POSTGRES_URL || ''
+const POSTGRES_URL =
+  process.env.POSTGRES_URL ||
+  'postgres://username:password@localhost:5432/default'
 
 export { POSTGRES_URL }
