954a8f26e1b83d0529464e7a680acd7a1508b10f
jgabriel1 <josegabriel@det.ufc.br>
2020-08-31 11:53:49 -0300

    Changed id fields to postgres' uuid field type.

--- src/database/migrations/1598878411863-CreateUser.ts
+++ src/database/migrations/1598878411863-CreateUser.ts
@@ -8,7 +8,7 @@ export default class CreateUser1598878411863 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
