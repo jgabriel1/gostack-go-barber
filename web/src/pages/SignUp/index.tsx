389ce306aaa5ce466352c703b517d3765aecc29a
jgabriel1 <josegabriel@det.ufc.br>
2020-09-03 23:00:05 -0300

    Installed unForm and applied it to SignUp form.

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi'
+import { Form } from '@unform/web'
 
 import Button from '../../components/Button'
 import Input from '../../components/Input'
@@ -9,13 +10,17 @@ import { Container, Content, Background } from './styles'
 import logo from '../../assets/logo.svg'
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: any): void {
+    console.log(data)
+  }
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={logo} alt="GoBarber" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} placeholder="Nome" />
@@ -28,7 +33,7 @@ const SignUp: React.FC = () => {
           />
 
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
 
         <a href="login">
           <FiArrowLeft />
