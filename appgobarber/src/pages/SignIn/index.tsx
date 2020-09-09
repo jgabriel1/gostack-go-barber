119c1cb4c9adb1d131b5bf0da0b4ced856d56b26
jgabriel1 <josegabriel@det.ufc.br>
2020-09-08 23:39:56 -0300

    Added icons and finished SignIn screen styling.

--- appgobarber/src/pages/SignIn/index.tsx
+++ appgobarber/src/pages/SignIn/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Image } from 'react-native'
 
-import { Container } from './styles'
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import { Container, Title } from './styles'
+
+import logoImg from '../../assets/logo.png'
 
 const SignIn: React.FC = () => {
   return (
     <Container>
-      <Text>SignIn</Text>
+      <Image source={logoImg} />
+      <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+
+      <Button
+        onPress={() => {
+          console.log('pressed enter')
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   )
 }
