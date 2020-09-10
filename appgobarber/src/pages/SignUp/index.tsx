01c128e339205f76e6b57dddded28d015ea6fc75
jgabriel1 <josegabriel@det.ufc.br>
2020-09-10 16:42:45 -0300

    Created SignUp screen.

--- appgobarber/src/pages/SignUp/index.tsx
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -1,13 +1,63 @@
 import React from 'react'
-import { Text } from 'react-native'
+import {
+  View,
+  Image,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
 
-import { Container } from './styles'
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles'
+
+import logoImg from '../../assets/logo.png'
 
 const SignUp: React.FC = () => {
   return (
-    <Container>
-      <Text>SignUp</Text>
-    </Container>
+    <>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        style={{ flex: 1 }}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('pressed enter')
+              }}
+            >
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignIn
+        onPress={() => {
+          console.log('Create Account')
+        }}
+      >
+        <Icon name="arrow-left" size={20} color="#fff" />
+        <BackToSignInText>Voltar para logon</BackToSignInText>
+      </BackToSignIn>
+    </>
   )
 }
 
