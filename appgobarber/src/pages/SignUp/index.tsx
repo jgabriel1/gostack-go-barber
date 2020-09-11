233e2b013aaf66150b34314089cc9f7d147d0cfc
José Gabriel Mourão Bezerra <62442043+jgabriel1@users.noreply.github.com>
2020-09-11 10:11:19 -0300

    Frontend mobile (#3)
    * Generated project and created code style configuration files.
    
    * Added react navigation and created first simple routes.
    
    * Added external fonts.
    
    * Added icons and finished SignIn screen styling.
    
    * Added rest of the buttons for SignIn and handled keyboard actions.
    
    * Created SignUp screen.
    
    * Added navigation between screens.
    
    * Hiding footer button on SignUp when keyboard is up.
    
    * Added unform form components to both screens and modified Input component.
    
    * Added usability features to inputs.
    
    * Finished forms validation.
    
    * Connected register form with backend.
    
    * Finished authetication routes.

---
+++ appgobarber/src/pages/SignUp/index.tsx
@@ -0,0 +1,169 @@
+import React, { useState, useEffect, useRef, useCallback } from 'react'
+import {
+  View,
+  Image,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+  TextInput,
+  Alert,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
+import { useNavigation } from '@react-navigation/native'
+import { Form } from '@unform/mobile'
+import { FormHandles } from '@unform/core'
+import * as Yup from 'yup'
+
+import getValidationErrors from '../../utils/getValidationErrors'
+import api from '../../services/api'
+
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles'
+
+import logoImg from '../../assets/logo.png'
+
+interface SignUpFormData {
+  name: string
+  email: string
+  password: string
+}
+
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null)
+  const emailInputRef = useRef<TextInput>(null)
+  const passwordInputRef = useRef<TextInput>(null)
+
+  const navigation = useNavigation()
+
+  // const [isKeybaordUp, setIsKeyboardUp] = useState(false)
+
+  // eslint-disable-next-line consistent-return
+  /* useEffect(() => {
+    if (Platform.OS === 'android') {
+      const show = Keyboard.addListener('keyboardDidShow', () => {
+        setIsKeyboardUp(current => !current)
+      })
+
+      const hide = Keyboard.addListener('keyboardDidHide', () => {
+        setIsKeyboardUp(current => !current)
+      })
+
+      return () => {
+        show.remove()
+        hide.remove()
+      }
+    }
+  }, []) */
+
+  const handleSignUp = useCallback(
+    async (data: SignUpFormData) => {
+      try {
+        formRef.current?.setErrors({})
+
+        const schema = Yup.object().shape({
+          name: Yup.string().required('Nome obrigatório.'),
+          email: Yup.string()
+            .required('E-mail obrigatório.')
+            .email('Digite um e-mail válido.'),
+          password: Yup.string().min(6, 'No mí­nimo 6 dí­gitos.'),
+        })
+
+        await schema.validate(data, { abortEarly: false })
+
+        await api.post('users', data)
+
+        Alert.alert(
+          'Cadastro realizado com sucesso!',
+          'Você já pode fazer seu logon no GoBarber.',
+        )
+
+        navigation.goBack()
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err)
+
+          formRef.current?.setErrors(Object.fromEntries(errors))
+          return
+        }
+
+        Alert.alert(
+          'Erro no cadastro.',
+          'Ocorreu um erro ao fazer cadastro, tente novamente.',
+        )
+      }
+    },
+    [navigation],
+  )
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        style={{ flex: 1 }}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Form ref={formRef} onSubmit={handleSignUp}>
+              <Input
+                autoCorrect
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="Nome"
+                returnKeyType="next"
+                onEndEditing={() => emailInputRef.current?.focus()}
+              />
+              <Input
+                ref={emailInputRef}
+                keyboardType="email-address"
+                autoCorrect={false}
+                autoCapitalize="none"
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                returnKeyType="next"
+                onEndEditing={() => passwordInputRef.current?.focus()}
+              />
+              <Input
+                ref={passwordInputRef}
+                secureTextEntry
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                textContentType="newPassword"
+                returnKeyType="send"
+                onEndEditing={() => formRef.current?.submitForm()}
+              />
+            </Form>
+
+            <Button onPress={() => formRef.current?.submitForm()}>
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      {!false && (
+        <BackToSignIn onPress={() => navigation.goBack()}>
+          <Icon name="arrow-left" size={20} color="#fff" />
+          <BackToSignInText>Voltar para logon</BackToSignInText>
+        </BackToSignIn>
+      )}
+    </>
+  )
+}
+
+export default SignUp
