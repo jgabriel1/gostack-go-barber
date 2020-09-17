428ec8ea7d46170a31e2c059b891dc4d931ca835
jgabriel1 <josegabriel@det.ufc.br>
2020-09-16 22:09:48 -0300

    Updated entities paths in typeorm config file.

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -6,7 +6,10 @@ module.exports = {
   name: "default",
   type: "postgres",
   url: process.env.POSTGRES_URL,
-  entities: ["./src/models/*.ts"],
+  entities: [
+    "./src/modules/appointments/infra/typeorm/entities/*",
+    "./src/modules/users/infra/typeorm/entities/*",
+  ],
   migrations: ["./src/database/migrations/*.ts"],
   cli: {
     migrationsDir: "./src/database/migrations"
