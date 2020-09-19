830c26cb5dfe1ee6127c7e91025049bdda12a701
jgabriel1 <josegabriel@det.ufc.br>
2020-09-19 13:13:40 -0300

    Created fake storage provider to be used in tests.

---
+++ server/src/shared/container/providers/StorageProvider/fakes/FakeStorageProvider.ts
@@ -0,0 +1,23 @@
+import IStorageProvider from '../models/IStorageProvider'
+
+export default class FakeStorageProvider implements IStorageProvider {
+  private storage: string[]
+
+  constructor() {
+    this.storage = []
+  }
+
+  public async saveFile(file: string): Promise<string> {
+    this.storage.push(file)
+
+    return file
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    const findIndex = this.storage.findIndex(
+      storageFile => storageFile === file,
+    )
+
+    this.storage.splice(findIndex, 1)
+  }
+}
