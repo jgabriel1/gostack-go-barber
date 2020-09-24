35046711d29e70ea875bc7d713303f3392ad6c3d
jgabriel1 <josegabriel@det.ufc.br>
2020-09-24 14:20:11 -0300

    Created service and tests for monthly availability for a provider.

---
+++ server/src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -0,0 +1,45 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
+import ListProviderMonthAvailabilityService from './ListProviderMonthAvailabilityService'
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository
+let listProviderMonthAvailability: ListProviderMonthAvailabilityService
+
+describe('ListProviderMonthAvailability', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository()
+    listProviderMonthAvailability = new ListProviderMonthAvailabilityService(
+      fakeAppointmentsRepository,
+    )
+  })
+
+  it('should be able to list all days available in a single month for a provider', async () => {
+    await fakeAppointmentsRepository.create({
+      provider_id: 'user-id',
+      date: new Date(2020, 4, 21, 8, 0, 0),
+    })
+
+    await Promise.all(
+      Array.from({ length: 10 }, (_, index) => index + 8).map(hour =>
+        fakeAppointmentsRepository.create({
+          provider_id: 'user-id',
+          date: new Date(2020, 4, 20, hour, 0, 0),
+        }),
+      ),
+    )
+
+    const availability = await listProviderMonthAvailability.execute({
+      provider_id: 'user-id',
+      year: 2020,
+      month: 5,
+    })
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { day: 19, available: true },
+        { day: 20, available: false },
+        { day: 21, available: true },
+        { day: 22, available: true },
+      ]),
+    )
+  })
+})
