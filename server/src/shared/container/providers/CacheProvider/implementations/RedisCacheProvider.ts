755be54db674347f636ffc66ab6d702b8770e47c
jgabriel1 <josegabriel@det.ufc.br>
2020-09-27 12:25:51 -0300

    Added invalidation of the provider list caching whenever a new provider is created.

--- server/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ server/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -29,4 +29,16 @@ export default class RedisCacheProvider implements ICacheProvider {
     // TODO
     await null
   }
+
+  public async invalidatePrefix(prefix: string): Promise<void> {
+    const keys = await this.client.keys(`${prefix}:*`)
+
+    const pipeline = this.client.pipeline()
+
+    keys.forEach(key => {
+      pipeline.del(key)
+    })
+
+    await pipeline.exec()
+  }
 }
