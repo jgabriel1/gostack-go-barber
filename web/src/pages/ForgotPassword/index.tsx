56705ab62ce4eb0705967f090d7b0f663f90ffe7
jgabriel1 <josegabriel@det.ufc.br>
2020-09-28 22:38:42 -0300

    Created forgot password route.

--- web/src/pages/ForgotPassword/index.tsx
+++ web/src/pages/ForgotPassword/index.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useCallback } from 'react'
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi'
+import { FiLogIn, FiMail } from 'react-icons/fi'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
 import { FormHandles } from '@unform/core'
@@ -9,28 +9,23 @@ import Button from '../../components/Button'
 import Input from '../../components/Input'
 
 import getValidationErrors from '../../utils/getValidationErrors'
-import { useAuth } from '../../hooks/auth'
 import { useToast } from '../../hooks/toast'
 
 import { Container, Content, AnimationContainer, Background } from './styles'
 
 import logo from '../../assets/logo.svg'
 
-interface SignInFormData {
+interface ForgotPasswordFormData {
   email: string
-  password: string
 }
 
-const SignIn: React.FC = () => {
+const ForgotPassword: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
-  const { signIn } = useAuth()
   const { addToast } = useToast()
 
-  const history = useHistory()
-
   const handleSubmit = useCallback(
-    async (data: SignInFormData) => {
+    async (data: ForgotPasswordFormData) => {
       try {
         formRef.current?.setErrors({})
 
@@ -38,17 +33,13 @@ const SignIn: React.FC = () => {
           email: Yup.string()
             .required('E-mail obrigatório.')
             .email('Digite um e-mail válido.'),
-          password: Yup.string().required('Senha obrigatória.'),
         })
 
         await schema.validate(data, { abortEarly: false })
 
-        await signIn({
-          email: data.email,
-          password: data.password,
-        })
+        // password recovery
 
-        history.push('/dashboard')
+        // history.push('/dashboard')
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
           const errors = getValidationErrors(err)
@@ -59,12 +50,13 @@ const SignIn: React.FC = () => {
 
         addToast({
           type: 'error',
-          title: 'Erro na autenticação.',
-          description: 'Ocorreu um erro ao fazer login, cheque as credenciais.',
+          title: 'Erro na recuperação de senha.',
+          description:
+            'Ocorreu um erro ao tentar recuperar a senha, tente novamente.',
         })
       }
     },
-    [signIn, addToast, history],
+    [addToast],
   )
 
   return (
@@ -74,24 +66,16 @@ const SignIn: React.FC = () => {
           <img src={logo} alt="GoBarber" />
 
           <Form ref={formRef} onSubmit={handleSubmit}>
-            <h1>Faça seu logon</h1>
+            <h1>Recuperar senha</h1>
 
             <Input name="email" icon={FiMail} placeholder="E-mail" />
-            <Input
-              name="password"
-              icon={FiLock}
-              type="password"
-              placeholder="Senha"
-            />
 
-            <Button type="submit">Entrar</Button>
-
-            <Link to="/forgot-password">Esqueci minha senha</Link>
+            <Button type="submit">Recuperar</Button>
           </Form>
 
           <Link to="signup">
             <FiLogIn />
-            Criar conta
+            Voltar ao login
           </Link>
         </AnimationContainer>
       </Content>
@@ -101,4 +85,4 @@ const SignIn: React.FC = () => {
   )
 }
 
-export default SignIn
+export default ForgotPassword
--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -5,6 +5,7 @@ import Route from './Route'
 
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
+import ForgotPassword from '../pages/ForgotPassword'
 import Dashboard from '../pages/Dashboard'
 
 const Routes: React.FC = () => {
@@ -12,6 +13,11 @@ const Routes: React.FC = () => {
     <Switch>
       <Route path="/" exact component={SignIn} isPrivate={false} />
       <Route path="/signup" component={SignUp} isPrivate={false} />
+      <Route
+        path="/forgot-password"
+        component={ForgotPassword}
+        isPrivate={false}
+      />
 
       <Route path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
