381772b725fabdcf84360b714df6a3a6bdb80683
jgabriel1 <josegabriel@det.ufc.br>
2020-09-29 13:43:28 -0300

    Created and styled main content for appointments list.

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import { FiPower } from 'react-icons/fi'
+import React, { useState } from 'react'
+import { FiClock, FiPower } from 'react-icons/fi'
 
-import { Container, Header, HeaderContent, Profile } from './styles'
+import {
+  Calendar,
+  Container,
+  Header,
+  HeaderContent,
+  Content,
+  Profile,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+} from './styles'
 
 import logoImg from '../../assets/logo.svg'
 import { useAuth } from '../../hooks/auth'
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date())
+
   const { signOut, user } = useAuth()
 
   return (
@@ -35,6 +48,94 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/62442043?s=460&v=4"
+                alt="Avatar"
+              />
+
+              <strong>José Gabriel</strong>
+
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/62442043?s=460&v=4"
+                  alt="Avatar"
+                />
+
+                <strong>Cliente</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/62442043?s=460&v=4"
+                  alt="Avatar"
+                />
+
+                <strong>Cliente</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/62442043?s=460&v=4"
+                  alt="Avatar"
+                />
+
+                <strong>Cliente</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+
+        <Calendar>Calendar</Calendar>
+      </Content>
     </Container>
   )
 }
