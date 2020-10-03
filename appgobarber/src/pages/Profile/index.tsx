5eb2effc2c3d5055fa1b63ebf2a448d7d1d410d6
jgabriel1 <josegabriel@det.ufc.br>
2020-10-02 23:01:14 -0300

    Able to use camera and search in gallery for a new avatar and send it to the backend.

--- appgobarber/src/pages/Profile/index.tsx
+++ appgobarber/src/pages/Profile/index.tsx
@@ -12,6 +12,7 @@ import { Form } from '@unform/mobile'
 import { FormHandles } from '@unform/core'
 import * as Yup from 'yup'
 import Icon from 'react-native-vector-icons/Feather'
+import ImagePicker from 'react-native-image-picker'
 
 import getValidationErrors from '../../utils/getValidationErrors'
 import { useAuth } from '../../hooks/auth'
@@ -115,6 +116,42 @@ const Profile: React.FC = () => {
     [navigation, updateUser],
   )
 
+  const handleUpdateAvatar = useCallback(() => {
+    ImagePicker.showImagePicker(
+      {
+        title: 'Selecione um avatar.',
+        cancelButtonTitle: 'Cancelar',
+        takePhotoButtonTitle: 'Usar câmera',
+        chooseFromLibraryButtonTitle: 'Escolher da galeria',
+      },
+      response => {
+        if (response.didCancel) {
+          return
+        }
+
+        if (response.error) {
+          Alert.alert('Erro ao atualizar seu avatar')
+          return
+        }
+
+        const data = new FormData()
+
+        data.append('avatar', {
+          type: 'image/jpeg',
+          name: `${user.id}.jpg`,
+          uri: response.uri,
+        })
+
+        api
+          .patch('/users/avatar', data)
+          .then(_response => {
+            updateUser(_response.data)
+          })
+          .catch(console.log)
+      },
+    )
+  }, [updateUser, user.id])
+
   const handleGoBack = useCallback(() => {
     navigation.goBack()
   }, [navigation])
@@ -135,7 +172,7 @@ const Profile: React.FC = () => {
               <Icon name="chevron-left" size={24} color="#999591" />
             </BackButton>
 
-            <UserAvatarButton onPress={() => signOut()}>
+            <UserAvatarButton onPress={handleUpdateAvatar}>
               <UserAvatar source={{ uri: user.avatar_url }} />
             </UserAvatarButton>
 
